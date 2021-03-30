4a9f1782e31d3642fdd9da56548c94ff36020638
armanziaei <armanziaei83@gmaio.com>
2021-03-30 20:16:16 +0430

    Initial commit

---
+++ app/src/main/java/com/example/daggerbinds/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.daggerbinds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
