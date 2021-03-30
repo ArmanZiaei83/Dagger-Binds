776f206704dfda3d827c76d731d6ad452902ab2b
armanziaei <armanziaei83@gmaio.com>
2021-03-30 20:25:00 +0430

    Feat Car Class : car class created

--- app/src/main/java/com/example/daggerbinds/Car/Car.java
+++ app/src/main/java/com/example/daggerbinds/Car/Car.java
@@ -1,2 +1,39 @@
-package com.example.daggerbinds.Car;public class Car {
+package com.example.daggerbinds.Car;
+
+import javax.inject.Inject;
+
+public class Car {
+
+    Engine engine;
+    Model model;
+
+    @Inject
+    public Car(Engine engine, Model model) {
+        this.engine = engine;
+        this.model = model;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Model getModel() {
+        return model;
+    }
+
+    public void setModel(Model model) {
+        this.model = model;
+    }
+
+    public void driveCar(){
+        makeSout("Car Is Driving . . . ");
+    }
+
+    private void makeSout(String message) {
+        System.out.println(message);
+    }
 }
